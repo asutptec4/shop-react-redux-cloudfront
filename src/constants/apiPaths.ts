f14c35a211a23637ddba953702c616f173fb30bc
asutptec4 <a.shishonok@gmail.com>
2023-03-10 21:06:14 +0300

    feat: integrate file upload

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://ea3875vmph.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://tilaqdib3h.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
