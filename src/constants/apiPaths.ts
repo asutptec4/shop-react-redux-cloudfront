7bb1544b703b49f6e3e7d94f91830ecb359f8bc9
asutptec4 <a.shishonok@gmail.com>
2023-04-12 11:28:48 +0300

    feat: use API gateway integration for https

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "http://asutptec4-cart-service-api.eu-west-1.elasticbeanstalk.com",
+  order: "https://hwlwtvqhf1.execute-api.eu-west-1.amazonaws.com",
   import: "https://ea3875vmph.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://tilaqdib3h.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "http://asutptec4-cart-service-api.eu-west-1.elasticbeanstalk.com",
+  cart: "https://hwlwtvqhf1.execute-api.eu-west-1.amazonaws.com",
 };
 
 export default API_PATHS;
