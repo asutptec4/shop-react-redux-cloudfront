cfe85ea5d3685686b08442040ef1f14c61dba486
asutptec4 <a.shishonok@gmail.com>
2023-04-12 08:09:46 +0300

    feat: integrate with ebs

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://chk36y3c2b.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "http://asutptec4-cart-service-api.eu-west-1.elasticbeanstalk.com",
   import: "https://ea3875vmph.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://tilaqdib3h.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://chk36y3c2b.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://asutptec4-cart-service-api.eu-west-1.elasticbeanstalk.com",
 };
 
 export default API_PATHS;
